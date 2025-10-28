cca008dbb234934e44dadc164ab074439fd7c23b
adamxrvn <adamxrvn@gmail.com>
2025-10-28 22:18:33 +0300

    S12 fixes

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,28 +1,13 @@
 # Демонстрационный TF с типовыми smell'ами для Checkov
 terraform {
   required_version = ">= 1.5.0"
-  
-    required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
-  }
 }
 
 provider "null" {}
 
 resource "null_resource" "demo" {
+  # no tags, no depends_on etc - просто болванка
   triggers = {
     always = timestamp()
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Environment = "demo"
-    ManagedBy   = "terraform"
   }
 }
